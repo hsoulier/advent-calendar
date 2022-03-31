8ae8948c0144c4ea0b4f90b63ba5fb699441a504
Juliette Bourgoin <juliette.bourgoin@mail-ecv.fr>
2022-03-31 13:28:10 +0200

    🗃️ Feat : Migrations

---
+++ database/migrations/2022_03_31_094619_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->string('thumbnail');
+            $table->string('origin');
+            $table->date('date');
+            $table->float('price');
+            $table->integer('calendar_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
