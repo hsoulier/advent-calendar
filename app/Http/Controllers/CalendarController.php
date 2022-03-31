1c0d5b9cf1bcd373bfb9a8f666b7a65c94aadcba
hsoulier <hi.soulier@gmail.com>
2022-03-31 23:01:02 +0200

    🔀  Fix: Part 2 of changing of renaming

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 
 class CalendarController extends Controller {
-    public function index() {
+    public function home() {
         $products = Product::where('calendar_id', 1)->get()->shuffle();
         return view('home', ['products' => $products]);
     }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [CalendarController::class, 'index'])->name('home');
+Route::get('/', [CalendarController::class, 'home'])->name('home');
 Route::get('/product/{id}', [ProductController::class, 'single'])->name('product');
 
 Route::get('/dashboard', function () {
