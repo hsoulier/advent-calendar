f9f3c187595ece424e9f8eda8e5eb283443bf773
Juliette Bourgoin <juliette.bourgoin@mail-ecv.fr>
2022-06-04 16:49:05 +0200

    💄 Fix : added CSS classes for the contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,23 @@
+<style>
+
+    .formulaire {
+        display: flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:center;
+        align-content:center;
+        margin: 50px auto 50px auto;
+    }
+
+    .mb-6 {
+        width: 40%;
+        padding: 20px;
+    }
+
+</style>
 <x-layout>
     <section style="min-height: 50vh;" class="bg-palette-green py-12 grid place-items-center place-content-center gap-8">
         <h1 class="text-6xl text-center font-bold">Nous contacter</h1>
-
-
     </section>
 
     <section>
@@ -16,26 +31,44 @@
 
 
 
-        <form action="{{ route('send-contact') }}" method="POST">
+        <form action="{{ route('send-contact') }}" method="POST" class="formulaire">
             @csrf
             @if (!Auth::check())
             <!-- si on est pas conncecté -->
-            <label>Nom</label>
-            <input type="text" name="name" id="name">
+            <div class="mb-6">
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nom</label>
+                <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </div>
 
-            <label>Email</label>
-            <input type="email" name="email" id="email">
+            <div class="mb-6">
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
+                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </div>
 
-            <label>Téléphone</label>
-            <input type="text" name="tel" id="tel">
+            <div class="mb-6">
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Téléphone</label>
+                <input type="text" name="tel" id="tel" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </div>
 
-            <label>Adresse</label>
-            <input type="text" name="adress" id="adress">
+            <div class="mb-6">
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Adresse</label>
+                <input type="text" name="adress" id="adress" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </div>
+
             @endif
             <!-- si on est conncecté, envoyer les infos du user directement dans le formulaire -->
+            @if (Auth::check())
+            <input type="text" name="name" id="name" :value="old('name')">
+            <input type="email" name="email" id="email" :value="old('email')">
+            <input type="text" name="tel" id="tel" :value="old('tel')">
+            <input type="text" name="adress" id="adress" :value="old('address')">
 
-            <label>Message</label>
-            <textarea name="message" id="message" rows="4"></textarea>
+            @endif
+            <div class="mb-6">
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Message</label>
+                <textarea name="message" id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+            </div>
+
 
 
 
@@ -43,7 +76,9 @@
 
             <!--<input type="text" name="hello" id="">-->
             <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-            <button type="submit">Evnoyer</button>
+            <button type="submit" class="bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Envoyer</button>
         </form>
+
+
     </section>
 </x-layout>
