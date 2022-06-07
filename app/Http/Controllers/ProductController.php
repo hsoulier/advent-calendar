ca37d975892bb9acfe3d303d55976748274ac369
ValouZ <57949313+ValouZ@users.noreply.github.com>
2022-06-07 23:30:52 +0200

    ✨ Block access to hidden products using the url

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,27 +9,33 @@ use Illuminate\Http\Request;
 use Spatie\LaravelMarkdown\MarkdownRenderer;
 use Illuminate\Support\Facades\Auth;
 
-
-
-
-class ProductController extends Controller {
-    public function single(Request $request) {
+class ProductController extends Controller
+{
+    public function single(Request $request)
+    {
         $product = Product::findOrFail($request->id);
-        $product->description = app(MarkdownRenderer::class)->toHtml($product->description);
+
+        // Bloquer l'accès aux produits dépassant la date d'aujd
+        if (new DateTime($product->date) > new DateTime()) {
+            return redirect()->route('home');
+        }
+        $product->description = app(MarkdownRenderer::class)->toHtml(
+            $product->description
+        );
 
         $comments = Comment::where(['product_id' => $request->id])
             ->get()
-            ->sortBy(fn ($el) => date_timestamp_get(new DateTime($el->date)));
-
+            ->sortBy(fn($el) => date_timestamp_get(new DateTime($el->date)));
 
         //dd($comments);
-        return view('product', ['product' => $product, 'comments' => $comments]);
+        return view('product', [
+            'product' => $product,
+            'comments' => $comments,
+        ]);
     }
 
-
-
-    public function send_comment(Request $request) {
-
+    public function send_comment(Request $request)
+    {
         $user = Auth::user();
 
         $values = $request->all();
@@ -42,25 +48,17 @@ class ProductController extends Controller {
             'date' => new DateTime()
         ]); */
 
-
-
         $product = Product::findOrFail($values['product_id']);
-
-
-
-
-
 
         $comm = Comment::create([
             //'user_id' => Auth::id(),
             'product_id' => intval($values['product_id']),
             'user_id' => $user->id,
             'text' => $values['comment'],
-            'date' => new DateTime()
+            'date' => new DateTime(),
         ]);
 
         //dd($comm);
-
 
         return redirect("/products/{$request->request->product_id}");
     }
